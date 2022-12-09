dfa9abf188f1b6d0225d8356c34eff138fe412dc
MindulaHewadewage <mindufernando@gmail.com>
2022-12-09 21:49:29 +0100

    js elementi dal dom

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-// Prendo l'elemento in pagina
-const passengerName = document.getElementById('passenger-name');
-const tripLenght = document.getElementById('trip-lenght');
-const userAge = document.getElementById('userAge');
-const button = document.getElementById('button');
+// Prendo elementi Form
+const passengerName = document.getElementById('name');
+const tripLenght = document.getElementById('kms');
+const userAge = document.getElementById('age');
+const cancelButton = document.getElementById('cancel');
+const continueButton = document.getElementById('continue');
+
+// Prendo elementi biglietto
+let nameValue = document.getElementById('name-value')
+let rateValue = document.getElementById('rate-value')
+let coachValue = document.getElementById('coach-value')
+let pnrValue = document.getElementById('pnr-value')
+let priceValue = document.getElementById('price-value')
+
