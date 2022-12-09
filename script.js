aca08fc7f2fcee6f93e75874fbccfa7cd92bd5f8
MindulaHewadewage <mindufernando@gmail.com>
2022-12-09 15:06:05 +0100

    sezione biglietto

--- script.js
+++ script.js
@@ -3,13 +3,3 @@ const passengerName = document.getElementById('passenger-name');
 const tripLenght = document.getElementById('trip-lenght');
 const userAge = document.getElementById('userAge');
 const button = document.getElementById('button');
-
-
-
-
-
-
-// Stampo il risultato
-button.addEventListener('click', function() {
-    passengerName.innerHTML = ('Nome');
-})
