6509d62816f52da74ba088996a149af5501bd62e
MindulaHewadewage <mindufernando@gmail.com>
2022-12-11 17:21:29 +0100

    sezione sconti

--- script.js
+++ script.js
@@ -6,9 +6,53 @@ const cancelButton = document.getElementById('cancel');
 const continueButton = document.getElementById('continue');
 
 // Prendo elementi biglietto
-let nameValue = document.getElementById('name-value')
-let rateValue = document.getElementById('rate-value')
-let coachValue = document.getElementById('coach-value')
-let pnrValue = document.getElementById('pnr-value')
-let priceValue = document.getElementById('price-value')
+const ticketSection = document.getElementById('ticket')
+const nameElement = document.getElementById('Element');
+const rateElement = document.getElementById('rate');
+const coachElement = document.getElementById('coach');
+const pnrElement = document.getElementById('pnr');
+const priceElement = document.getElementById('price');
+
+// Aggancio l'event listener al bottone
+continueButton.addEventListener('click' , function(){
+    // Recupero i valori del form
+    const nameValue = passengerName.value.trim();
+    const kmsValue = tripLenght.value;
+    const ageValue = userAge.value;
+
+    // validazione
+
+    if(!nameValue || isNaN(kmsValue)){
+        return;
+    }
+
+
+    // calcolo del prezzo in base ai kms
+    let rateName = 'Tariffa Ordinaria'
+    let price = 0.21 * kmsValue;
+
+
+    // verifico se devo applicare sconti
+    if (ageValue === 'under'){
+        price = 0.8;
+        rateName = 'Tariffa Giovani'
+
+
+    }
+})
+
+
+
+
+
+
+
+// arrotondo a due decimali
+
+// svuoto i campi
+
+// riempio tutti i punti coi valori corretti
+
+// mostro il biglietto(rimuovo d-none)
+
 
